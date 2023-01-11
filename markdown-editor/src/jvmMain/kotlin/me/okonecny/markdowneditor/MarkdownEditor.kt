d1ab21593954b91d57618b96a233c0ad79078d63
Ondřej Konečný <konecny.ondrej@gmail.com>
2023-01-11 22:45:34 +0100

    No line newlines in paragraphs.

--- markdown-editor/src/jvmMain/kotlin/me/okonecny/markdowneditor/MarkdownEditor.kt
+++ markdown-editor/src/jvmMain/kotlin/me/okonecny/markdowneditor/MarkdownEditor.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.appendInlineContent
+import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import org.intellij.markdown.MarkdownElementTypes
 import org.intellij.markdown.MarkdownElementTypes.ATX_1
 import org.intellij.markdown.MarkdownElementTypes.ATX_2
@@ -26,7 +26,9 @@ import org.intellij.markdown.MarkdownElementTypes.SETEXT_1
 import org.intellij.markdown.MarkdownElementTypes.SETEXT_2
 import org.intellij.markdown.MarkdownElementTypes.UNORDERED_LIST
 import org.intellij.markdown.MarkdownTokenTypes
+import org.intellij.markdown.MarkdownTokenTypes.Companion.EOL
 import org.intellij.markdown.MarkdownTokenTypes.Companion.HORIZONTAL_RULE
+import org.intellij.markdown.MarkdownTokenTypes.Companion.TEXT
 import org.intellij.markdown.ast.ASTNode
 
 /**
@@ -37,6 +39,7 @@ fun MarkdownEditor(sourceText: String, documentTheme: DocumentTheme = DocumentTh
     val markdown = remember { MarkdownEditorComponent::class.create() }
     val parser = remember { markdown.parser }
     val markdownRoot = parser.buildMarkdownTreeFromString(sourceText)
+    // TODO: make the source or the AST be state so we can edit.
 
     CompositionLocalProvider(
         LocalDocumentTheme provides documentTheme
@@ -59,7 +62,8 @@ fun MarkdownEditor(sourceText: String, documentTheme: DocumentTheme = DocumentTh
 @Composable
 private fun RenderedNode(node: ASTNode, sourceText: String) {
     val styles = DocumentTheme.current.styles
-    when (node.type) { // Only block elements should be here.
+    when (node.type) {
+        // Blocks
         MARKDOWN_FILE -> File(node, sourceText) // Not really used.
         ATX_1 -> AtxHeader(node, sourceText, styles.h1)
         SETEXT_1 -> SetextHeader(node, sourceText, styles.h1)
@@ -71,9 +75,9 @@ private fun RenderedNode(node: ASTNode, sourceText: String) {
         ATX_6 -> AtxHeader(node, sourceText, styles.h6)
         PARAGRAPH -> Paragraph(node, sourceText)
         CODE_BLOCK -> CodeBlock(node, sourceText)
-        ORDERED_LIST -> MarkdownList(node, sourceText)
-        UNORDERED_LIST -> MarkdownList(node, sourceText)
-        LIST_ITEM -> MarkdownListItem(node, sourceText)
+        ORDERED_LIST -> OrderedList(node, sourceText)
+        UNORDERED_LIST -> UnorderedList(node, sourceText)
+        LIST_ITEM -> ListItem(node, sourceText)
         HORIZONTAL_RULE -> HorizontalRule()
     }
 }
@@ -95,18 +99,24 @@ private fun SetextHeader(headerNode: ASTNode, sourceText: String, headerStyle: T
     Text(headerContent.text(sourceText).trim(), style = headerStyle)
 }
 
+private enum class WhitespaceModes {
+    PRE,     // Render all whitespace as is.
+    NO_EOL
+}
+private val LocalWhitespaceMode = compositionLocalOf { WhitespaceModes.PRE }
+
 @Composable
 private fun Paragraph(paragraphNode: ASTNode, sourceText: String) {
-    buildAnnotatedString {
-        appendInlineContent("test")
+    CompositionLocalProvider(
+        LocalWhitespaceMode provides WhitespaceModes.NO_EOL
+    ) {
+        val parsedContent = parseInlineContent(paragraphNode, sourceText)
+        Text(
+            parsedContent.text,
+            inlineContent = parsedContent.inlineTextContent,
+            style = DocumentTheme.current.styles.paragraph
+        )
     }
-    Text(
-        // TODO: Use AnnotatedString and InlineTextContent, Ignore newlines and indents.
-        paragraphNode
-            .text(sourceText)
-            .replace(Regex("[\\s|\n]+"), " "),
-        style = DocumentTheme.current.styles.paragraph
-    )
     // TODO: inline elements (images, links, highlights)
 }
 
@@ -118,16 +128,22 @@ private fun CodeBlock(blockNode: ASTNode, sourceText: String) {
     )
 }
 
+private enum class ListTypes {
+    ORDERED,
+    UNORDERED
+}
+private val ListType = compositionLocalOf { ListTypes.ORDERED }
 private val ListNumber = compositionLocalOf { 0 }
 
 @Composable
-private fun MarkdownList(listNode: ASTNode, sourceText: String) {
+private fun OrderedList(listNode: ASTNode, sourceText: String) {
     Column {
         var currentListItem = 0
         listNode.children.forEach { itemNode ->
             if (itemNode.type == LIST_ITEM) currentListItem++
             CompositionLocalProvider(
-                ListNumber provides currentListItem
+                ListNumber provides currentListItem,
+                ListType provides ListTypes.ORDERED
             ) {
                 RenderedNode(itemNode, sourceText)
             }
@@ -136,12 +152,32 @@ private fun MarkdownList(listNode: ASTNode, sourceText: String) {
 }
 
 @Composable
-fun MarkdownListItem(itemNode: ASTNode, sourceText: String) {
+private fun UnorderedList(listNode: ASTNode, sourceText: String) {
+    Column {
+        listNode.children.forEach { itemNode ->
+            CompositionLocalProvider(
+                ListType provides ListTypes.UNORDERED
+            ) {
+                RenderedNode(itemNode, sourceText)
+            }
+        }
+    }
+}
+
+@Composable
+private fun ListItem(itemNode: ASTNode, sourceText: String) {
     Row {
-        Text(
-            "${ListNumber.current}. ", // TODO: unordered
-            style = DocumentTheme.current.styles.listNumber
-        )
+        when (ListType.current) {
+            ListTypes.ORDERED -> Text(
+                "${ListNumber.current}. ",
+                style = DocumentTheme.current.styles.listNumber
+            )
+            ListTypes.UNORDERED -> Text(
+                "\u2022 ", // TODO: parse the LIST_BULLET
+                style = DocumentTheme.current.styles.listNumber
+            )
+        }
+
         Column {
             itemNode.children.forEach { itemContentNode ->
                 RenderedNode(itemContentNode, sourceText)
@@ -158,4 +194,43 @@ fun HorizontalRule() {
             .then(Modifier.height(lineStyle.width))
             .then(Modifier.border(lineStyle))
     )
+}
+
+private data class MarkdownParsedInline(
+    val text: AnnotatedString,
+    val inlineTextContent: Map<String, InlineTextContent>
+)
+
+typealias IdToInlineContent = MutableMap<String, InlineTextContent>
+
+@Composable
+private fun parseInlineContent(blockNode: ASTNode, sourceText: String): MarkdownParsedInline {
+    val inlineContent: IdToInlineContent = mutableMapOf()
+    val annotatedStringBuilder = AnnotatedString.Builder()
+
+    blockNode.children.forEach { child ->
+        renderInlineNode(child, sourceText, annotatedStringBuilder, inlineContent)
+    }
+
+    return MarkdownParsedInline(
+        annotatedStringBuilder.toAnnotatedString(),
+        inlineContent
+    )
+}
+
+@Composable
+private fun renderInlineNode(
+    inlineNode: ASTNode,
+    sourceText: String,
+    asBuilder: AnnotatedString.Builder,
+    inlineContent: IdToInlineContent
+) {
+    when(inlineNode.type) {
+        TEXT -> asBuilder.append(inlineNode.text(sourceText))
+        EOL -> when(LocalWhitespaceMode.current) {
+            WhitespaceModes.PRE -> asBuilder.append("\n")
+            WhitespaceModes.NO_EOL -> asBuilder.append(" ")
+        }
+
+    }
 }

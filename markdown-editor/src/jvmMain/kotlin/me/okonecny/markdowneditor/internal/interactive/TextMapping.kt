07fe168e6603a7b857c69332b5998a215ec10215
Ondřej Konečný <konecny.ondrej@gmail.com>
2023-03-20 21:53:04 +0100

    Add text mapping to convert from and to source offsets.

---
+++ markdown-editor/src/jvmMain/kotlin/me/okonecny/markdowneditor/internal/interactive/TextMapping.kt
@@ -0,0 +1,41 @@
+package me.okonecny.markdowneditor.internal.interactive
+
+import androidx.compose.ui.text.TextRange
+
+interface TextMapping {
+    fun toSource(visualTextRange: TextRange): TextRange
+    fun toVisual(sourceTextRange: TextRange): TextRange
+}
+
+class ConstantTextMapping(
+    private val sourceTextRange: TextRange,
+    private val visualTextRange: TextRange = TextRange.Zero
+) : TextMapping {
+    override fun toSource(visualTextRange: TextRange): TextRange = sourceTextRange
+    override fun toVisual(sourceTextRange: TextRange): TextRange = visualTextRange
+}
+
+class SourcePaddedTextMapping(
+    private val startSourcePadding: TextRange,
+    private val endSourcePadding: TextRange
+) : TextMapping {
+    override fun toSource(visualTextRange: TextRange): TextRange {
+        val hitsSourceStart = visualTextRange.start == 0
+        val sourceEnd = startSourcePadding.end + visualTextRange.length
+        val hitsSourceEnd = sourceEnd >= endSourcePadding.start
+        return TextRange(
+            if (hitsSourceStart) startSourcePadding.start
+            else startSourcePadding.end + visualTextRange.start,
+            if (hitsSourceEnd) endSourcePadding.end
+            else sourceEnd
+        )
+    }
+
+    override fun toVisual(sourceTextRange: TextRange): TextRange {
+        val start = (sourceTextRange.start - startSourcePadding.end).coerceAtLeast(0)
+        val maxLength = endSourcePadding.start - startSourcePadding.end
+        val end = start + sourceTextRange.length.coerceAtMost(maxLength)
+        return TextRange(start, end)
+    }
+// TODO: -1 / +1 anywhere?
+}

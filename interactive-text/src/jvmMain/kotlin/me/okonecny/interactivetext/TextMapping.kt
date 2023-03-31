4942d0808cf95c2e55ff5c41b8f52dc278387a6a
Ondřej Konečný <konecny.ondrej@gmail.com>
2023-03-31 23:03:14 +0200

    Remove useless SourcePaddedTextMapping

--- interactive-text/src/jvmMain/kotlin/me/okonecny/interactivetext/TextMapping.kt
+++ interactive-text/src/jvmMain/kotlin/me/okonecny/interactivetext/TextMapping.kt
@@ -42,31 +42,6 @@ class ConstantTextMapping(
     }
 }
 
-class SourcePaddedTextMapping(
-    private val startSourcePadding: TextRange,
-    private val endSourcePadding: TextRange
-) : TextMapping {
-    override fun toSource(visualTextRange: TextRange): TextRange {
-        val hitsSourceStart = visualTextRange.start == 0
-        val sourceEnd = startSourcePadding.end + visualTextRange.length
-        val hitsSourceEnd = sourceEnd >= endSourcePadding.start
-        return TextRange(
-            if (hitsSourceStart) startSourcePadding.start
-            else startSourcePadding.end + visualTextRange.start,
-            if (hitsSourceEnd) endSourcePadding.end
-            else sourceEnd
-        )
-    }
-
-    override fun toVisual(sourceTextRange: TextRange): TextRange {
-        val start = (sourceTextRange.start - startSourcePadding.end).coerceAtLeast(0)
-        val maxLength = endSourcePadding.start - startSourcePadding.end
-        val end = start + sourceTextRange.length.coerceAtMost(maxLength)
-        return TextRange(start, end)
-    }
-// TODO: -1 / +1 anywhere?
-}
-
 class ChunkedSourceTextMapping(
     private val chunks: List<TextMapping>
 ) : TextMapping {

d4bb065a6b1aa3f43b19e5c02c45e50315f2535c
Ondřej Konečný <konecny.ondrej@gmail.com>
2023-05-05 18:50:50 +0200

    Remove unused import in main

--- src/jvmMain/kotlin/me/okonecny/whodoes/Main.kt
+++ src/jvmMain/kotlin/me/okonecny/whodoes/Main.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp

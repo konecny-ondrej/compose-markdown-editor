3a35d6c4215fd3717be58b7951c1cac24207f43c
Ondřej Konečný <konecny.ondrej@gmail.com>
2023-05-05 18:51:16 +0200

    Remove unused import in App

--- src/jvmMain/kotlin/me/okonecny/whodoes/App.kt
+++ src/jvmMain/kotlin/me/okonecny/whodoes/App.kt
@@ -2,14 +2,11 @@ package me.okonecny.whodoes
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.useResource
-import androidx.compose.ui.unit.dp
 import me.okonecny.interactivetext.rememberInteractiveScope
 import me.okonecny.markdowneditor.DocumentTheme
 import me.okonecny.markdowneditor.MarkdownEditor

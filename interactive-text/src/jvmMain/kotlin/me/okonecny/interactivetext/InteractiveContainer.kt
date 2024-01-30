7afb972aa605c481d23cc4ab8d59b771d5d49018
Ondřej Konečný <konecny.ondrej@gmail.com>
2024-01-30 23:45:46 +0100

    Must draw selection under the content, because it would paint over all nested components, including toolbar, otherwise.

--- interactive-text/src/jvmMain/kotlin/me/okonecny/interactivetext/InteractiveContainer.kt
+++ interactive-text/src/jvmMain/kotlin/me/okonecny/interactivetext/InteractiveContainer.kt
@@ -2,6 +2,7 @@ package me.okonecny.interactivetext
 
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
@@ -142,7 +143,6 @@ private fun Modifier.paintSelection(
             )
         }
 
-        drawContent()
         drawPath(combinedSelectionPath, selectionStyle.fillColor)
         drawPath(
             combinedSelectionPath,
@@ -152,6 +152,7 @@ private fun Modifier.paintSelection(
                 join = StrokeJoin.Round
             )
         )
+        drawContent()
     }
 
 private fun TextLayoutResult.getFilledPathForRange(start: Int, end: Int, growBy: Float = 1f): Path {

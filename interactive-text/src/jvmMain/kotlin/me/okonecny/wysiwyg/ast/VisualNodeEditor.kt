304582e45067cbbee48589ebb083e0a7dcc576e1
Ondřej Konečný <konecny.ondrej@gmail.com>
2024-10-09 16:48:28 +0200

    Decide that VisualNodeEditor will edit one node, not the whole document.

--- interactive-text/src/jvmMain/kotlin/me/okonecny/wysiwyg/ast/VisualNodeEditor.kt
+++ interactive-text/src/jvmMain/kotlin/me/okonecny/wysiwyg/ast/VisualNodeEditor.kt
@@ -3,19 +3,19 @@ package me.okonecny.wysiwyg.ast
 import me.okonecny.interactivetext.TextEditCommand
 
 /**
- * Edits a document represented by its root node.
+ * Edits a document node.
  */
 interface VisualNodeEditor<T : Any> {
     /**
      * Edit the node based on what the user wants.
-     * @param rootNode The root node of the document to edit.
+     * @param node The node to edit.
      * @param command What the user wants.
      * @param editorState Overall editor state.
      * @return The edited node, which will be used instead of the old node in its parent. Return this for no edits,
      * return null to delete the node.
      */
     fun edit(
-        rootNode: VisualNode<T>,
+        node: VisualNode<T>,
         command: TextEditCommand,
         editorState: VisualEditorState
     ): VisualNode<T>?
@@ -23,10 +23,10 @@ interface VisualNodeEditor<T : Any> {
     companion object {
         fun <T : Any> noop(): VisualNodeEditor<T> = object : VisualNodeEditor<T> {
             override fun edit(
-                rootNode: VisualNode<T>,
+                node: VisualNode<T>,
                 command: TextEditCommand,
                 editorState: VisualEditorState
-            ): VisualNode<T> = rootNode
+            ): VisualNode<T> = node
         }
     }
 }

b9b3c251894b8fb0448f7a70d2b25bccb75600fe
Ondřej Konečný <konecny.ondrej@gmail.com>
2025-01-14 16:14:21 +0100

    Detailed comment for CommandEditor

--- interactive-text/src/jvmMain/kotlin/me/okonecny/wysiwyg/edit/CommandEditor.kt
+++ interactive-text/src/jvmMain/kotlin/me/okonecny/wysiwyg/edit/CommandEditor.kt
@@ -3,6 +3,15 @@ package me.okonecny.wysiwyg.edit
 import me.okonecny.interactivetext.TextInputCommand
 import me.okonecny.wysiwyg.WysiwygEditorState
 
+/**
+ * Applies the text input commands to the editor / the AST.
+ */
 interface CommandEditor<C : TextInputCommand> {
+    /**
+     * Applies the changes specified by the command to the editor.
+     * @param editorState Current state of the editor.
+     * @param command Input command to apply.
+     * @return New editor state with the changes applied. Null if no changes were made.
+     */
     fun <D : Any> edit(editorState: WysiwygEditorState<D>, command: C): WysiwygEditorState<D>?
 }

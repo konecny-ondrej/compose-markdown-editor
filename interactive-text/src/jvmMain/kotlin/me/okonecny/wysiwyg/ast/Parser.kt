2baa595a44cb64f587cdb32cf48f7ed4fb39362a
Ondřej Konečný <konecny.ondrej@gmail.com>
2025-05-07 16:47:35 +0200

    Doc for Parser.parse

--- interactive-text/src/jvmMain/kotlin/me/okonecny/wysiwyg/ast/Parser.kt
+++ interactive-text/src/jvmMain/kotlin/me/okonecny/wysiwyg/ast/Parser.kt
@@ -6,5 +6,11 @@ import java.nio.file.Path
  * Parses the input into our internal AST represented by VisualNode instances.
  */
 interface Parser<in IN, Document : Any> {
+
+    /**
+     * Parses the whole document. There are two use cases for this method:
+     * 1. Parsing of the whole document.
+     * 2. Parsing of a markdown fragment when pasting from clipboard.
+     */
     fun parse(input: IN, basePath: Path): VisualNode<Document, Document>
 }

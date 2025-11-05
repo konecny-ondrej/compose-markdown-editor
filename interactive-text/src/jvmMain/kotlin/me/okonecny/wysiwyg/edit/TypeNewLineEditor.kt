3d91a45d7019bd2eb51fc48412ca24484148c2bc
Ondřej Konečný <konecny.ondrej@gmail.com>
2025-11-05 18:02:07 +0100

    Make generic new line editor work

--- interactive-text/src/jvmMain/kotlin/me/okonecny/wysiwyg/edit/TypeNewLineEditor.kt
+++ interactive-text/src/jvmMain/kotlin/me/okonecny/wysiwyg/edit/TypeNewLineEditor.kt
@@ -2,8 +2,27 @@ package me.okonecny.wysiwyg.edit
 
 import me.okonecny.interactivetext.NewLine
 import me.okonecny.wysiwyg.WysiwygEditorState
+import me.okonecny.wysiwyg.ast.VisualNode
+import me.okonecny.wysiwyg.ast.VisualNodeCursorPosition
 
 class TypeNewLineEditor<D : Any> : CommandEditor<NewLine, D> {
-    override fun edit(editorState: WysiwygEditorState<D>, command: NewLine): WysiwygEditorState<D>? =
-        TODO("Create a new empty paragraph or a new bullet point in a list.")
+    override fun edit(editorState: WysiwygEditorState<D>, command: NewLine): WysiwygEditorState<D>? {
+        val editorStateWithoutSelection = editorState.withSelectionDeleted
+        val cursor = editorStateWithoutSelection.nodeCursor ?: return null
+        val textNodeUnderCursor = cursor.textNodeUnderCursor
+
+        val editedNode = textNodeUnderCursor.node.replaceWith(
+            VisualNode(textNodeUnderCursor.node.data.replaceText(
+                textNodeUnderCursor.text.take(textNodeUnderCursor.charOffset) + "\n" + textNodeUnderCursor.text.drop(textNodeUnderCursor.charOffset)
+            ))
+        )
+        return editorStateWithoutSelection.edit(
+            newVisualDocument = editedNode.root,
+            newCursor = VisualNodeCursorPosition(
+                editedNode,
+                textNodeUnderCursor.charOffset + 1
+            ),
+            newSelection = null
+        )
+    }
 }

515c3e07f112f9257fcd3874ed6134e22c2bd0ec
Ondřej Konečný <konecny.ondrej@gmail.com>
2026-02-09 00:32:33 +0100

    Migrate the text input to the new api

--- interactive-text/src/jvmMain/kotlin/me/okonecny/interactivetext/TextInput.kt
+++ interactive-text/src/jvmMain/kotlin/me/okonecny/interactivetext/TextInput.kt
@@ -1,48 +1,137 @@
 package me.okonecny.interactivetext
 
-import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.input.key.*
-import androidx.compose.ui.platform.LocalTextInputService
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.platform.PlatformTextInputMethodRequest
+import androidx.compose.ui.platform.PlatformTextInputModifierNode
+import androidx.compose.ui.platform.establishTextInputSession
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.*
-import co.touchlab.kermit.Logger
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-fun Modifier.textInput(
+fun Modifier.compositeTextInput(
     onInput: (TextInputCommand) -> Unit
-): Modifier = composed {
-    val handleInput by rememberUpdatedState(onInput)
-    var textInputSession by remember { mutableStateOf<TextInputSession?>(null) }
-    val textInputService = LocalTextInputService.current
+): Modifier = then(object : ModifierNodeElement<MarkdownEditorTextInputModifierNode>() {
+    override fun create(): MarkdownEditorTextInputModifierNode = MarkdownEditorTextInputModifierNode(onInput)
 
-    if (textInputSession == null && textInputService != null) {
-        Logger.d("Start text input.")
-        textInputSession = textInputService.startInput(
-            value = TextFieldValue(""),
-            imeOptions = ImeOptions.Default,
-            onEditCommand = { editCommands ->
-                editCommands.forEach { command: EditCommand ->
-                    when (command) {
-                        is CommitTextCommand -> handleInput(Type(command.text))
-                        is BackspaceCommand -> handleInput(Delete(Delete.Direction.BEFORE_CURSOR, Delete.Size.LETTER))
-                        // Is any other command relevant? See subclasses of EditCommand.
-                    }
-                }
-            },
-            onImeActionPerformed = { _ -> })
+    override fun update(node: MarkdownEditorTextInputModifierNode) {
+        node.launchInput(onInput)
     }
-    DisposableEffect(Unit) {
-        onDispose {
-            val session = textInputSession
-            if (session != null && textInputService != null) {
-                Logger.d("Stop text input.")
-                textInputService.stopInput(session)
-                textInputSession = null
+
+    override fun hashCode(): Int = onInput.hashCode()
+
+    override fun equals(other: Any?): Boolean =
+        if (other is MarkdownEditorTextInputModifierNode) other.onInput == onInput else false
+})
+
+data class MarkdownEditorTextInputModifierNode(
+    val onInput: (TextInputCommand) -> Unit
+) : PlatformTextInputModifierNode, Modifier.Node() {
+    private var inputSessionJob: Job? = null
+
+    fun launchInput(handleInput: (TextInputCommand) -> Unit) {
+        // TODO: only launch the input session if the editor is focused.
+        inputSessionJob = coroutineScope.launch {
+            establishTextInputSession {
+                startInputMethod(object : PlatformTextInputMethodRequest {
+                    @ExperimentalComposeUiApi
+                    override val value: () -> TextFieldValue = { TextFieldValue() }
+
+                    @ExperimentalComposeUiApi
+                    override val state: TextEditorState = object : TextEditorState {
+                        override val selection: TextRange = TextRange.Zero
+                        override val composition: TextRange? = null
+                        override val length: Int = 0
+
+                        override fun get(index: Int): Char {
+                            throw IllegalStateException()
+                        }
+
+                        override fun subSequence(startIndex: Int, endIndex: Int): CharSequence {
+                            throw IllegalStateException()
+                        }
+                    }
+
+                    @ExperimentalComposeUiApi
+                    override val imeOptions: ImeOptions = ImeOptions.Default
+
+                    @ExperimentalComposeUiApi
+                    override val onEditCommand: (List<EditCommand>) -> Unit = { editCommands ->
+                        editCommands.forEach { command: EditCommand ->
+                            when (command) {
+                                is CommitTextCommand -> handleInput(Type(command.text))
+                                is BackspaceCommand -> handleInput(
+                                    Delete(
+                                        Delete.Direction.BEFORE_CURSOR,
+                                        Delete.Size.LETTER
+                                    )
+                                )
+                                // Is any other command relevant? See subclasses of EditCommand.
+                            }
+                        }
+                    }
+
+                    @ExperimentalComposeUiApi
+                    override val onImeAction: ((ImeAction) -> Unit)? = null
+
+                    @ExperimentalComposeUiApi
+                    override val textLayoutResult: () -> TextLayoutResult? = { null }
+
+                    @ExperimentalComposeUiApi
+                    override val focusedRectInRoot: () -> Rect? = { null }
+
+                    @ExperimentalComposeUiApi
+                    override val textFieldRectInRoot: () -> Rect? = { null }
+
+                    @ExperimentalComposeUiApi
+                    override val textClippingRectInRoot: () -> Rect? = { null }
+
+                    @ExperimentalComposeUiApi
+                    override val editText: (block: TextEditingScope.() -> Unit) -> Unit = { block ->
+                        object : TextEditingScope {
+                            // FIXME: emulate the methods properly so the system thinks this is an actual input field.
+                            override fun deleteSurroundingTextInCodePoints(
+                                lengthBeforeCursor: Int,
+                                lengthAfterCursor: Int
+                            ) {
+                                TODO("Not yet implemented")
+                            }
+
+                            override fun commitText(text: CharSequence, newCursorPosition: Int) {
+                                if (text.isNotEmpty()) handleInput(Type(text.toString()))
+                            }
+
+                            override fun setComposingText(text: CharSequence, newCursorPosition: Int) {
+                                TODO("Not yet implemented")
+                            }
+
+                            override fun finishComposingText() {
+                                TODO("Not yet implemented")
+                            }
+                        }.block()
+//                        textState = TextFieldValue(
+//                            text = mutable.text,
+//                            selection = mutable.selection,
+//                            composition = mutable.composition
+//                        )
+                    }
+                })
             }
         }
     }
 
-    return@composed onKeyEvent { keyEvent: KeyEvent ->
+}
+
+fun Modifier.textInput(
+    onInput: (TextInputCommand) -> Unit
+): Modifier = this
+    .compositeTextInput(onInput)
+    .onKeyEvent { keyEvent: KeyEvent ->
         if (keyEvent.type == KeyEventType.KeyDown) {
             val typedChar = keyEvent.utf16CodePoint.toChar()
             if (typedChar > Char.MIN_VALUE && typedChar < Char.MAX_VALUE && !typedChar.isISOControl()) {
@@ -86,7 +175,6 @@ fun Modifier.textInput(
         }
         return@onKeyEvent false
     }
-}
 
 sealed interface TextInputCommand {
     val needsValidCursor: Boolean

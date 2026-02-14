d5788ad8336cd0a295f0b98524535ac12f5c3a57
Ondřej Konečný <konecny.ondrej@gmail.com>
2026-02-14 22:44:48 +0100

    Fix typing newline in Markdown

--- markdown-editor/src/jvmMain/kotlin/me/okonecny/markdowneditor/edit/MarkdownNewLineEditor.kt
+++ markdown-editor/src/jvmMain/kotlin/me/okonecny/markdowneditor/edit/MarkdownNewLineEditor.kt
@@ -20,58 +20,78 @@ class MarkdownNewLineEditor<D : Any> : CommandEditor<NewLine, D> {
 
         val forbidNewLineIn = linkedSetOf(
             TableCell::class,
-            Link::class,
-            AutoLink::class,
+            AutoLink::class, // TODO: make autolinks splittable? How? Convert them to ordinary links?
             TableCell::class
         )
         if (textNodeUnderCursor.node.findClosestParentMatching { it.data::class in forbidNewLineIn } != null) return null
 
-        val interestingParents = linkedSetOf(
-            BulletList::class,
-            OrderedList::class,
+        val splittableDataClasses = linkedSetOf(
+            BulletListItem::class,
+            OrderedListItem::class,
+            TaskListItem::class,
             CodeBlock::class,
-            Paragraph::class
+            Paragraph::class,
+            Heading::class
         )
 
-        val closestInterestingParent = // TODO: search for the closest interesting parent in order (there is a paragraph in a list item)
-            textNodeUnderCursor.node.findClosestParentMatching { it.data::class in interestingParents }
-        val interestingParentContainer = closestInterestingParent?.parent
+        val closestSplittableNode = splittableDataClasses.firstNotNullOfOrNull { splittableDataClass ->
+            if (textNodeUnderCursor.node.data::class == splittableDataClass) {
+                textNodeUnderCursor.node
+            } else {
+                textNodeUnderCursor.node.findClosestParentMatching { it.data::class == splittableDataClass }
+            }
+        }
+
+        val splittableNodeParent = closestSplittableNode?.parent
             ?: return null // Cannot split the root, we would have to create a new root somehow.
 
-        val parentIndexInContainer = closestInterestingParent.indexIn(interestingParentContainer)
+        val splittableNodeIndexInContainer = closestSplittableNode.indexIn(splittableNodeParent)
             ?: throw IllegalStateException("The child node was not found in its subtree. Was it removed?")
-        val offsetInParentTextData = closestInterestingParent.findOffsetByTextChild(textNodeUnderCursor)
+        val offSetInSplittableNodeData = closestSplittableNode.findOffsetByTextChild(textNodeUnderCursor)
 
-        val splitChildren = when (closestInterestingParent.data) {
+        val splitChildren = when (closestSplittableNode.data) {
             is HasText -> {
-                val textData = closestInterestingParent.data as HasText
+                val textData = closestSplittableNode.data as HasText
                 listOf(
                     VisualNode(
                         textData.replaceText(
-                            textData.text.take(offsetInParentTextData) + System.lineSeparator() + textData.text.drop(
-                                offsetInParentTextData
+                            textData.text.take(offSetInSplittableNodeData) + System.lineSeparator() + textData.text.drop(
+                                offSetInSplittableNodeData
                             )
                         )
                     )
                 )
             }
 
-            else -> closestInterestingParent.split(offsetInParentTextData)
+            else -> closestSplittableNode.split(offSetInSplittableNodeData)
                 .toList()
         }
 
-        val editedParentContainer = interestingParentContainer.replaceWith(
-            interestingParentContainer.copy(
-                proposedChildren = interestingParentContainer.children.take(parentIndexInContainer)
+        val editedParentContainer = splittableNodeParent.replaceWith(
+            splittableNodeParent.copy(
+                proposedChildren = splittableNodeParent.children.take(splittableNodeIndexInContainer)
                         + splitChildren
-                        + interestingParentContainer.children.drop(parentIndexInContainer + 1)
+                        + splittableNodeParent.children.drop(splittableNodeIndexInContainer + 1)
             )
         )
-        val newSecondSplitChild = editedParentContainer.children[parentIndexInContainer + 1]
+
+        val childNodeWasSplit = splitChildren.size > 1
+        val childNodeAfterNewline = if (childNodeWasSplit) {
+            editedParentContainer.children[splittableNodeIndexInContainer + 1]
+        } else {
+            editedParentContainer.children[splittableNodeIndexInContainer]
+        }
 
         return editorStateWithoutSelection.edit(
             newVisualDocument = editedParentContainer.root,
-            newCursor = VisualNodeCursorPosition(newSecondSplitChild, 0),
+            newCursor = VisualNodeCursorPosition(
+                containerNode = childNodeAfterNewline,
+                visualOffset = if (childNodeWasSplit) {
+                    0
+                } else {
+                    offSetInSplittableNodeData + 1
+                }
+            ),
             newSelection = null
         )
     }
